49cc7455421a74726fd5a999d3361b4a0f1c9365
kemboi22 <kemboielvis22@gmail.xom>
2023-07-17 12:26:32 +0300

    Added VButton.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
 
 import VInput from "./components/VInput.vue";
+import VButton from "./components/VButton.vue";
 </script>
 
 <template>
   <VInput label="Email" rounded type="email"/>
+  <VButton label="Button" rounded type="button">
+    +
+  </VButton>
 </template>
 
 <style scoped>
---
+++ src/components/VButton.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+
+defineProps({
+  type: {type: String, required: true},
+  label: {type: String, required: true},
+  bgColor: {
+    type: String, required: false, default: "bg-blue-500 text-white hover:bg-blue-600 focus:outline-\n" +
+        "      none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm\n" +
+        "      dark:focus:ring-offset-gray-800"
+  },
+  rounded: {type: Boolean, required: false, default: false}
+
+})
+</script>
+
+<template>
+  <button
+      :class="[bgColor, rounded ? 'rounded-full' : '']"
+      class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border
+      border-transparent font-semibold ">
+    {{ label }}
+    <slot/>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
